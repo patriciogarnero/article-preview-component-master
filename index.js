70bc2c0bbb1213b90554698003ae9dcf8a94eda9
pat <patriciogarnero@gmail.com>
2021-07-21 08:20:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+  let buttons = document.querySelectorAll(".arrow_icon");
+
+  for (var i = 0; i < buttons.length; i ++) {
+      buttons[i].addEventListener("click", function() {
+      document.querySelector(".share_sheet").classList.toggle("hidden");
+  });
+  };
